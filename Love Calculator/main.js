5e007d1d97aff062a1ad3a04c261afcdc9989951
dr-winner <duvorrichardwinner@gmail.com>
2024-11-03 08:47:31 +0000

    working on love calculator

--- Love Calculator/main.js
+++ Love Calculator/main.js
@@ -1,15 +1,24 @@
-function calculateLove(){
+function calculateLove() {
     const firstName = document.getElementById('firstName').value.trim();
     const secondName = document.getElementById('secondName').value.trim();
 
-    if(firstName === '' || secondName === ''){
+    if (firstName === '' || secondName === '') {
         alert('Please enter both names');
-    }else{
+    } else {
         const lovePercentage = Math.floor(Math.random() * 101);
 
         const result = document.getElementById('result');
 
         result.innerHTML = `${firstName} and ${secondName} are ${lovePercentage}% compatible`;
+        if (lovePercentage < 30) {
+            result.innerHTML += "<br>You seem not to be a great match though";
+        } else if (lovePercentage >= 30 && lovePercentage < 70) {
+            result.innerHTML += "<br>There is so much potential. Go give it a try";
+        } else if (lovePercentage >= 70 && lovePercentage < 100) {
+            result.innerHTML += "<br>It is a love match. You are going to be together";
+        } else {
+            result.innerHTML += "<br>It is a perfect match. You are soulmates";
+        }
     }
 }
 
